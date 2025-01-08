a51337701c49ee7573ecee3f270ccf81d1b36247
kokis1 <reuben.stannah@icloud.com>
2025-01-08 21:06:59 +0000

    wrote a grammar for the simple version of the language

---
+++ second_attempt/main.py
@@ -0,0 +1,80 @@
+import scanner
+from sys import argv
+
+
+class main():
+    def scan_from_command_line(self):
+        '''allows the language to be used from the command line'''
+        
+        # starts the text as anything other than "quit"
+        text = 0
+        
+        while text != "quit":            
+            # sets the text as the prompt
+            text = input("> ")
+            
+            # resets the position of the scanner
+            self.scan.position = 0
+            
+            # scans the input text and adds the new tokens to the array
+            self.scan.main(text)
+            
+            
+            # prints out the token array
+            for i in range(len(self.scan.token_array)):
+                print(self.scan.token_array[i])
+    
+    def get_file_as_array_of_lines(self, filepath):
+        '''opens the file and reads the lines of it'''
+        
+        
+        with open(filepath, "r", newline="\n") as file:
+            
+            text = list(file.readlines())
+            
+            # removes the "\n" newline character from the end of each entry in the array of lines
+            text = [line.strip("\n") for line in text]
+        return text
+    
+    def scan_from_file(self, filepath):
+        '''reads the file from the filepath specified in the fucntion'''
+        
+        #reads the file form the filepath into an appropriate format
+        text = self.get_file_as_array_of_lines(filepath)
+        
+        # iterates throught each line and adds the tokens from that to the array of tokens
+        for i in range(len(text)):
+            # updates the position to zero for the start of the new line
+            self.scan.position = 0
+            
+            # updates the line number as well
+            self.scan.line += 1
+            
+            # if this is the last line in the file then the EOF token is added to the end of the array
+            if i == len(text) - 1:
+                self.scan.is_last_line = True
+            
+            # renews what line we are reading from
+            line = text[i]
+            
+            # runs the main function of scan, which scans from the file and produces an array of tokens
+            self.scan.main(line)
+            
+        # prints out the token array
+        for i in range(len(self.scan.token_array)):
+            print(self.scan.token_array[i])
+        print("there are ", self.scan.line + 1, "lines in this file")
+    
+    def main(self):
+        # initialises a new scanner object
+        self.scan = scanner.scanner()
+        
+        
+        if len(argv) == 1:
+            self.scan_from_command_line()
+        elif len(argv) == 2:
+            self.scan_from_file(str(argv[1]))
+
+
+program = main()
+program.main()
---
+++ second_attempt/scanner.py
@@ -0,0 +1,170 @@
+########################
+## this is where      ##
+## all the different  ##
+## types of value go  ##
+########################
+IF = "IF"
+ELSE = "ELSE"
+WHILE = "WHILE"
+INT = "INT"
+INTEGER = "INTEGER"
+BOOL = "BOOL"
+BOOL_LITERAL = "BOOL_LITERAL"
+STRING = "STRING"
+STR_LITERAL = "STR_LITERAL"
+RETURN = "RETURN"
+IDENTIFIER = "IDENTIFIER"
+FN = "FN"
+COLON = "COLON"
+S_COLON = "S_COLON"
+L_BRACKET = "L_BRACKET"
+R_BRACKET = "R_BRACKET"
+L_PAREN = "L_PAREN"
+R_PAREN = "R_PAREN"
+AND = "AND"
+OR = "OR"
+NOT = "NOT"
+PLUS = "PLUS"
+MINUS = "MINUS"
+TIMES = "TIMES"
+DIVIDE = "DIVIDE"
+LESS = "LESS"
+GREATER = "GREATER"
+EQUAL = "EQUAL"
+QUOTATION = "QUOTATION"
+EOF = "EOF"
+
+
+class tok():
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+    def __str__(self):
+        return "TOKEN({type}, {value})".format(type=self.type, value=self.value)
+    
+    def __repr__(self):
+        return self.__str__()
+
+
+class scanner:
+    def __init__(self):
+        self.position = 0
+        self.line = -1
+        self.current_char = 0
+        self.token_array = []
+        self.is_last_line = False
+        
+    def peek_next_char(self):
+        new_position = self.position + 1
+        return self.text[new_position]
+    
+    def advance(self):
+        if self.position < len(self.text) - 1:
+            self.position += 1
+            self.current_char = self.text[self.position]
+        else:
+            self.current_char = None
+            
+    def skip_line(self):
+        self.line += 1
+        
+    def get_number(self):
+        result = ""
+        while self.current_char is not None and (self.current_char.isdigit() or self.current_char == "-"):
+            result += self.current_char
+            self.advance()
+        return int(result)
+    
+    def get_word(self):
+        result = ""
+        while self.current_char is not None and self.current_char.isalpha():
+            result += self.current_char
+            self.advance()
+        return result
+    
+    def get_string_literal(self):
+        result = ""
+        self.advance()
+        print("getting string")
+        while self.current_char != '"':
+            result += self.current_char
+            self.advance()
+        self.advance()
+        return result
+    
+    def scan(self):
+        '''my simple take on the tokeniser'''
+        
+        while True:
+            if self.current_char == None:
+                break
+            elif self.current_char == " ":
+                '''if the char is a space, we continue'''
+                self.advance()
+                continue
+            elif self.current_char == '"':
+                '''gets the value of any string literal between double quotes'''
+                self.token_array.append(tok(QUOTATION, 0))
+                str_literal = self.get_string_literal()
+                self.token_array.append(tok(STR_LITERAL, str_literal))
+                self.token_array.append(tok(QUOTATION, 0))
+                continue
+            elif self.current_char.isdigit():
+                '''if the current char is a digit, we read until it isn't a digit'''
+                integer = self.get_number()
+                self.token_array.append(tok(INTEGER, integer))
+                continue
+            elif self.current_char.isalpha():
+                '''this is for words'''
+                word = self.get_word()
+                if word in ("if", "else", "elif", "while", "int", "bool", "string", "return",
+                            "fn", "and", "or","not"):
+                    token = tok(word.upper(), 0)
+                    self.token_array.append(token)
+                elif word in ("true", "false"):
+                    token = tok(BOOL_LITERAL, word.upper())
+                    self.token_array.append(token)
+                else:
+                    token = tok(IDENTIFIER, word)
+                    self.token_array.append(token)
+            elif self.current_char == ":":
+                self.token_array.append(tok(COLON, 0))
+                
+            elif self.token_array == ";":
+                self.token_array.append(tok(S_COLON, 0))
+                
+            elif self.current_char == "+":
+                self.token_array.append(tok(PLUS, 0))
+                
+            elif self.current_char == "-":
+                if self.peek_next_char().isdigit():
+                    number = self.get_number()
+                    self.token_array.append(tok(INTEGER, number))
+                else:
+                    self.token_array.append(tok(MINUS, 0))
+                    
+            elif self.current_char == "(":
+                self.token_array.append(tok(L_PAREN, 0))
+                
+            elif self.current_char == ")":
+                self.token_array.append(tok(R_PAREN, 0))
+                
+            elif self.current_char == "{":
+                self.token_array.append(tok(L_BRACKET, 0))
+                
+            elif self.current_char == "}":
+                self.token_array.append(tok(R_BRACKET, 0))
+            
+            elif self.current_char == "=":
+                self.token_array.append(tok(EQUAL, 0))
+            self.advance()
+        
+        if self.is_last_line:
+            self.token_array.append(tok(EOF, 0))
+                
+    
+    
+    def main(self, text):
+        self.text = text
+        self.current_char = self.text[self.position]
+        self.scan()
